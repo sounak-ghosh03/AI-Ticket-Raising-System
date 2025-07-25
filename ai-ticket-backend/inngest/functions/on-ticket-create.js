608ae4c4dc885749eadc7e43272fbc91289ebf4b
SOUNAK GHOSH <sounak2309@gmail.com>
2025-07-25 21:34:36 +0530

    bug fixes

--- ai-ticket-backend/inngest/functions/on-ticket-create.js
+++ ai-ticket-backend/inngest/functions/on-ticket-create.js
@@ -1,6 +1,9 @@
 import { inngest } from "../client.js";
 import Ticket from "../../models/ticket.model.js";
 import User from "../../models/user.model.js";
+import { NonRetriableError } from "inngest";
+import { sendEmail } from "../../utils/mailer.js";
+import analyzeTicket from "../../utils/ai.js";
 
 export const onTicketCreated = inngest.createFunction(
     { id: "on-ticket-created", retries: 2 },
@@ -9,7 +12,7 @@ export const onTicketCreated = inngest.createFunction(
         try {
             const { ticketId } = event.data;
 
-            //fetch the ticket details from database
+            //fetch ticket from databse
             const ticket = await step.run("fetch-ticket", async () => {
                 const ticketObject = await Ticket.findById(ticketId);
                 if (!ticket) {
@@ -33,7 +36,6 @@ export const onTicketCreated = inngest.createFunction(
                         )
                             ? "medium"
                             : aiResponse.priority,
-
                         helpfullNotes: aiResponse.helpfullNotes,
                         status: "IN_PROGRESS",
                         relatedSkills: aiResponse.relatedSkills,
@@ -47,14 +49,16 @@ export const onTicketCreated = inngest.createFunction(
                 let user = await User.findOne({
                     role: "moderator",
                     skills: {
-                        $elementMatch: {
+                        $elemMatch: {
                             $regex: relatedSkills.join("|"),
                             $options: "i",
                         },
                     },
                 });
                 if (!user) {
-                    user = await User.findOne({ role: "admin" });
+                    user = await User.findOne({
+                        role: "admin",
+                    });
                 }
                 await Ticket.findByIdAndUpdate(ticket._id, {
                     assignedTo: user?._id || null,
@@ -67,15 +71,15 @@ export const onTicketCreated = inngest.createFunction(
                     const finalTicket = await Ticket.findById(ticket._id);
                     await sendEmail(
                         moderator.email,
-                        "New ticket",
-                        `A new ticket has been assigned to you ${finalTicket.title}.`
+                        "Ticket Assigned",
+                        `A new ticket is assigned to you ${finalTicket.title}`
                     );
                 }
             });
 
             return { success: true };
-        } catch (error) {
-            console.error("Error running the steps", error.message);
+        } catch (err) {
+            console.error(" Error running the step", err.message);
             return { success: false };
         }
     }
